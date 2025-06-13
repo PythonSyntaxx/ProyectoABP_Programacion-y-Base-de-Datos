9e5c86f5f7a2d4466d73d76f75de107c976e580c
AxelCortez11 <rodrigocortez803@gmail.com>
2025-06-12 22:07:16 -0300

    codigo datos.py

---
+++ SRC/datos.py
@@ -0,0 +1,12 @@
+# datos.py
+roles = [
+    {"id_rol": 1, "rol": "admin"},
+    {"id_rol": 2, "rol": "estandar"}
+]
+
+usuarios = [
+    {"id_user": 1, "nombre": "admin", "email": "admin@gmail.com", "password": "Admin1", "id_rol": 1},
+    {"id_user": 2, "nombre": "estandar", "email": "estandar@gmail.com", "password": "abc123", "id_rol": 2}
+]
+
+id_user_counter = 2
